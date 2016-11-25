2a330e1cf6d935d715a1177a90b877898eb29b5b
Rodrigo Lima <contato@homebrewbrasil.com.br>
2016-11-25 00:25:52 -0200

    teste home

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use template index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
+
