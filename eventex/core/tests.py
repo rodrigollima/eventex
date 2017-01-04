6c38023d3bc8cacad2b71b688f0aac7a3621038a
Rodrigo Lima <contato@homebrewbrasil.com.br>
2017-01-04 21:12:07 -0200

    Adiciona teste na home

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -5,6 +5,7 @@ class HomeTest(TestCase):
         self.response = self.client.get('/')
 
     def test_get(self):
+        """GET / must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
